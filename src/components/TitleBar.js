f2437434fcb487bf485580f030d8942fa709488a
MrWillCom <mr.will.com@outlook.com>
2021-08-09 08:40:28 +0000

    Fix path in TitleBar.js

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import os from 'os'
 
-import Win32TitleBar from './win32TitleBar'
+import Win32TitleBar from './Win32TitleBar'
 import DragArea from './DragArea'
 
 export default () => {
